98cd2e63e5acc215347226331c9649659bb9416a
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-01-18 16:35:32 -0500

    Update Shooter.cpp

--- src/main/cpp/Shooter.cpp
+++ src/main/cpp/Shooter.cpp
@@ -5,5 +5,5 @@ Shooter::Shooter() {
   hood = new WPI_TalonSRX(0);
 }
 
-void Shooter::MoveMotor(bool dir, double speed) { motor->Set((dir) ? speed : -speed)); }
-void Shooter::MoveHood(bool dir, double speed) { hood->Set((dir) ? speed : -speed)); }
+void Shooter::RunShooter(double speed) { shooter->Set(speed); }
+void Shooter::RunHood(double speed) { hood->Set(speed); }
