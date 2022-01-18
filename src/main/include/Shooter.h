e6e8d7faf7653e0c0fe6c040b41c6debb87cd461
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-01-18 16:25:43 -0500

    Update Shooter.h

--- src/main/include/Shooter.h
+++ src/main/include/Shooter.h
@@ -1,1 +1,13 @@
+#include <ctre/Phoenix.h>
 
+class Shooter {
+public:
+  Shooter();
+  ~Shooter();
+  
+  void MoveMotor(bool dir, double speed);
+  void MoveHood(bool dir, double speed);
+  
+private:
+  WPI_TalonSRX *motor, *hood;
+};
