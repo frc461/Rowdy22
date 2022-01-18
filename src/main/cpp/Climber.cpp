104742104c6fdfcd70c56c1c9a8c6c11efe9c369
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-01-18 16:10:29 -0500

    Update Climber.cpp

--- src/main/cpp/Climber.cpp
+++ src/main/cpp/Climber.cpp
@@ -8,14 +8,10 @@ Climber::Climber() {
     speed = 0.75;
 }
 
-void Climber::MoveHangMotor(bool dir) {
-    hangMotor->Set((dir) ? speed : -speed;
-}
+void Climber::MoveHangMotor(bool dir) { hangMotor->Set((dir) ? speed : -speed; }
+void Climber::MoveHookMotor(bool dir) { hookMotor->Set((dir) ? speed : -speed; }
+void Climber::MoveSwingMotor(bool dir) { swingMotor->Set((dir) ? speed : -speed; }
 
-void Climber::MoveHookMotor(bool dir) {
-    hookMotor->Set((dir) ? speed : -speed;
-}
-
-void Climber::MoveSwingMotor(bool dir) {
-    swingMotor->Set((dir) ? speed : -speed;
-}
+double GetHookEncoder() { return hookMotor->GetSelectedSensorPosition(); }
+double GetHangEncoder() { return hangMotor->GetSelectedSensorPosition(); }
+double GetSwingEncoder() { return swingMotor->GetSelectedSensorPosition(); }
