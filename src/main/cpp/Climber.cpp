bae8e1090189112de2e6939e2d9e2aebd411ae30
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-01-18 16:04:32 -0500

    Update Climber.cpp

--- src/main/cpp/Climber.cpp
+++ src/main/cpp/Climber.cpp
@@ -4,4 +4,18 @@ Climber::Climber() {
     hangMotor = new WPI_TalonSRX(0);
     hookMotor = new WPI_TalonSRX(0);
     swingMotor = new WPI_TalonSRX(0);
+    
+    speed = 0.75;
+}
+
+void Climber::MoveHangMotor(bool dir) {
+    hangMotor->Set((dir) ? speed : -speed;
+}
+
+void Climber::MoveHookMotor(bool dir) {
+    hookMotor->Set((dir) ? speed : -speed;
+}
+
+void Climber::MoveSwingMotor(bool dir) {
+    swingMotor->Set((dir) ? speed : -speed;
 }
