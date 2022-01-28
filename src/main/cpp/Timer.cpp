c3f87f1da14833003b71a953089db1dff5aa0fa6
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-01-28 13:11:49 -0500

    Update Timer.cpp

--- src/main/cpp/Timer.cpp
+++ src/main/cpp/Timer.cpp
@@ -11,6 +11,12 @@ void Timer::Start() { timer->Start(); }
 
 bool Timer::SecondsPassed(double t) {
   if (!firstCall) {
-    
+    initTime = timer->getFPGATimestamp();
+    firstCall = true;
   }
+  if (timer->HasPeriodPassed(initTime + t)) {
+    firstCall = false;
+    return true;
+  }
+  return false;
 }
