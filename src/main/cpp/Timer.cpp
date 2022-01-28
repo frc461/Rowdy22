6f64e2d0dcc9128f88596cd9ed5c09a3cf350e60
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-01-28 13:07:06 -0500

    Update Timer.cpp

--- src/main/cpp/Timer.cpp
+++ src/main/cpp/Timer.cpp
@@ -1,5 +1,16 @@
 #include "Timer.h"
 
 Timer::Timer() {
+  timer = new Timer();
   
+  firstCall = false;
+  initTime = 0.0;
+}
+
+void Timer::Start() { timer->Start(); }
+
+bool Timer::SecondsPassed(double t) {
+  if (!firstCall) {
+    
+  }
 }
