8154f65976660a288fd01d445b94f144902c0a89
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-02-18 13:21:47 -0500

    Update Vision.cpp

--- src/main/cpp/Vision.cpp
+++ src/main/cpp/Vision.cpp
@@ -2,15 +2,31 @@
 
 Vision::Vision() {
     auto instance = nt::NetworkTableInstance::GetDefault();
-    cam = instance.GetTable("limelight");
+    limelight = instance.GetTable("limelight");
+    
+    backCam = new cs::UsbCamera("backCam", 0);
+    backCam->SetResolution(320,240);
+    output = CameraServer::GetInstance()->PutVideo("backCam",320,240);
+    sink = new cs::CvSink("cvsink");
+    sink->SetSource(*backCam);
+    frameData = NULL;
 }
 
 Vision::Values Vision::GetValues() {
-    values = { cam->GetNumber("tx",0.0), cam->GetNumber("ty",0.0), cam->GetNumber("ta",0.0) };
+    values = { limelight->GetNumber("tx",0.0), limelight->GetNumber("ty",0.0), limelight->GetNumber("ta",0.0) };
     return values;
 }
 
-void Vision::SetCamState(bool state) {
-    cam->PutNumber("ledMode", (state) ? 3 : 1);
-    cam->PutNumber("camMode", (state) ? 0 : 1);
+void Vision::SetLimelightState(bool state) {
+    limelight->PutNumber("ledMode", (state) ? 3 : 1);
+    limelight->PutNumber("camMode", (state) ? 0 : 1);
+}
+
+void Vision::SetBackCamState(bool state) {
+    if (state) {
+        cvSink->GetFrame(frameData);
+        if (!frameData.empty()) {
+            output->PutFrame(frameData);
+        }
+    }
 }
