6c7d288a4aa60c5e4d66b9bb98c6bcb148f0dde2
WilmKwn <43156054+WilmKwn@users.noreply.github.com>
2022-03-07 19:41:50 -0500

    Update Conveyor.cpp

--- src/main/cpp/Conveyor.cpp
+++ src/main/cpp/Conveyor.cpp
@@ -6,9 +6,20 @@ Conveyor::Conveyor(){
 
     topSensor = new frc::DigitalInput(4);
     bottomSensor = new frc::DigitalInput(5);
+    
+    nBalls = 0;
+    out = in = false;
 }
 
 void Conveyor::RunMotor(double speed) { motor->Set(speed); }
 void Conveyor::RunHold(bool dir) { hold->Set(dir); }
 
 bool Conveyor::GetBallSensorState(bool top) { return (top) ? !topSensor->Get() : !bottomSensor->Get(); }
+
+double Conveyor::GetNumBalls() {
+    if (!in && GetBallSensorState(false)) { nBalls++; in = true; }
+    else if (in && !GetBallSensorState(false)) { in = false; }
+    
+    if (!out && GetBallSensorState(true)) { out = true; }
+    else if (out && !GetBallSensorState(true)) { nBalls--; out = false; }
+}
